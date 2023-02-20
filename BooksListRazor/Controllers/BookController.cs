728b75e15d81e05fe224bd5cc5cfd2d817fb4cf9
HP <HP@DESKTOP-VP5V6ES>
2023-02-20 10:26:25 +0200

    Add project files.

---
+++ BooksListRazor/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using BooksListRazor.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksListRazor.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Books.ToList();
+            return View(result);
+        }
+    }
+}
---
+++ BooksListRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BooksListRazor.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksListRazor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
